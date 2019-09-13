8e63b1f34b34fbb884666a0773a2b25de5973877
namhd1992 <stpnghd.vn@gmail.com>
2019-09-13 15:40:13 +0700

    fix

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3005;
+const port = process.env.PORT || 3008;
 const path = require('path');
 const fs = require('fs')
 
